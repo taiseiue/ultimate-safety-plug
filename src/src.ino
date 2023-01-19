808b7b8bb489be31be5a60133f43c6a791134f1c
たい <taisei@wsoft.ws>
2023-01-19 15:39:57 +0900

    遠隔操作機能つくった

--- src/src.ino
+++ src/src.ino
@@ -3,8 +3,40 @@
 #include <WebServer.h>  //サーバー設定用
 
 // アクセスポイント設定
-const char ssid_AP[] = "logikara";  //SSID
-const char pass_AP[] = "atomlite";  //パスワード(8文字以上)
+const char ssid_AP[] = "ultimate-safety-plug";  //SSID
+const char pass_AP[] = "prozemi-ie";  //パスワード(8文字以上)
+
+// FastLEDライブラリの設定（CRGB構造体）
+CRGB dispColor(uint8_t r, uint8_t g, uint8_t b) {
+  return (CRGB)((r << 16) | (g << 8) | b);
+}
+// LEDマトリクス表示指定配列（0:消灯、1以上：色指定配列で指定した色に指定（color配列1次要素番号））
+int ok[5][5] = {{0,0,3,0,0},
+                    {0,3,0,3,0},
+                    {3,0,0,0,3},
+                    {0,3,0,3,0},
+                    {0,0,3,0,0}}; //十字と4色表示
+
+int ng[5][5] =     {{1,0,0,0,1},
+                    {0,1,0,1,0},
+                    {0,0,1,0,0},
+                    {0,1,0,1,0},
+                    {1,0,0,0,1}}; //十字と4色表示
+
+int off[5][5] =    {{0,0,0,0,0},
+                    {0,0,0,0,0},
+                    {0,0,0,0,0},
+                    {0,0,0,0,0},
+                    {0,0,0,0,0}}; //十字と4色表示
+
+// 色指定配列（1次要素を色番号として表示する色を2次要素の3原色{赤、緑、青}で設定）
+//       LED色： {{0:消灯}  , {1:ピンク}   , {2:オレンジ}  , {3:グリーン} , {4:パープル}  , {5:ホワイト}}
+int color[][3] = {{0, 0, 0}, {255, 0, 00}, {255, 255, 0}, {70, 255, 0}, {70, 0, 255}, {255, 255, 255}};
+
+// 変数宣言
+int num = 0;  //matrix配列の色番号格納用
+
+
 
 // サーバー設定ポート80で接続
 WebServer server(80);
@@ -12,19 +44,20 @@ WebServer server(80);
 char html[] = "\
 <!DOCTYPE html><html lang=\"jp\"><head>\n\
 <meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\
-<title>REMOTE-SWITCH</title>\n\
+<title>アルティメットセーフティプラグ</title>\n\
 </head><body>\n\
-  <h1>REMOTE-SWITCH</h1>\n\
+  <h1>アルティメットセーフティプラグ遠隔操作</h1>\n\
   <div>\n\
-    <button style=\"height:50px\" onclick=\"location.href='/get/btn_on'\">　ON　</button>\n\
-    <button style=\"height:50px\" onclick=\"location.href='/get/btn_off'\">　OFF　</button>\n\
+    <button style=\"height:50px\" onclick=\"location.href='/get/btn_on'\">ブザーオン</button>\n\
+    <button style=\"height:50px\" onclick=\"location.href='/get/btn_off'\">ブザーオフ</button>\n\
+    <br/>\n\
+    <button style=\"height:50px\" onclick=\"location.href='/get/mx_ok'\">マトリクスOK</button>\n\
+    <button style=\"height:50px\" onclick=\"location.href='/get/mx_ng'\">マトリクスNG</button>\n\
+    <button style=\"height:50px\" onclick=\"location.href='/get/mx_off'\">マトリクス消灯</button>\n\
   </div>\n\
 </body></html>\n";
 
-// FastLED（CRGB構造体）設定
-CRGB dispColor(uint8_t r, uint8_t g, uint8_t b) {
-  return (CRGB)((r << 16) | (g << 8) | b);
-}
+
 //-------------------------------------------------
 // サーバーリクエスト時処理関数
 //-------------------------------------------------
@@ -38,16 +71,45 @@ void handleNotFound() {
 }
 // ブラウザONボタン処理
 void btnOn() {
-  digitalWrite(21, HIGH);                   //出力0 ON（青色LED）
-  M5.dis.drawpix(0, dispColor(0, 0, 200));  //本体LED（青）
+  digitalWrite(33, HIGH);                   //出力0 ON（青色LED）
   server.send(200, "text/html", html);  //レスポンス200を返しhtml送信
 }
 // ブラウザOFFボタン処理
 void btnOff() {
-  digitalWrite(21, LOW);                    //出力0 OFF（青色LED）
-  M5.dis.drawpix(0, dispColor(20, 20, 20)); //本体LED（白）
+  digitalWrite(33, LOW);                    //出力0 OFF（青色LED）
   server.send(200, "text/html", html);  //レスポンス200を返しhtml送信
 }
+
+void mx_ok() {
+  drawMatrix(ok);
+  server.send(200, "text/html", html);  //レスポンス200を返しhtml送信
+}
+void mx_ng() {
+  drawMatrix(ng);
+  server.send(200, "text/html", html);  //レスポンス200を返しhtml送信
+}
+void mx_off() {
+  drawMatrix(off);
+  server.send(200, "text/html", html);  //レスポンス200を返しhtml送信
+}
+
+// マトリクス描画処理
+void drawMatrix(int matrix[5][5]){
+  digitalWrite(33, HIGH);
+  delay(10);
+  digitalWrite(33, LOW);
+  for (int i = 0; i < 5; i++) {     //matrix行ループ
+    for (int j = 0; j < 5; j++) {   //matrix列ループ
+      
+      num = matrix[i][j];           //matrix配列の色番号取得
+      //matrix行ごと（0,5,10,15,20）に左から右へ色番号の色で表示
+      M5.dis.drawpix(i*5+j, dispColor(color[num][0], color[num][1], color[num][2]));
+      M5.dis.setBrightness(255);
+      
+    }
+  }
+}
+
 //-------------------------------------------------
 // 初期設定
 //-------------------------------------------------
@@ -63,6 +125,9 @@ void setup() {
   server.onNotFound(handleNotFound);  //Webページが見つからない時の応答関数を設定
   server.on("/get/btn_on", btnOn);    //ボタンオン受信処理
   server.on("/get/btn_off", btnOff);  //ボタンオフ受信処理
+  server.on("/get/mx_ok", mx_ok);  //ボタンオフ受信処理
+  server.on("/get/mx_ng", mx_ng);  //ボタンオフ受信処理
+  server.on("/get/mx_off", mx_off);  //ボタンオフ受信処理
   server.begin();                     //Webサーバー開始
 
   // シリアル出力
@@ -74,7 +139,7 @@ void setup() {
   // 本体LED設定
   M5.dis.drawpix(0, dispColor(20, 20, 20)); //LED点灯（白）
   // 入出力端子設定
-  pinMode(21, OUTPUT);  //出力端子
+  pinMode(33, OUTPUT);  //出力端子
 }
 //-------------------------------------------------
 // メイン
